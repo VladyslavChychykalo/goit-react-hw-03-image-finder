4f97155f34b2ccecac1da5bed75ab69ec5cb1751
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-11-16 15:45:02 +0200

    Updates

---
+++ precache-manifest.67e0fe8a675895745a0a1159abc79b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e6d9d5b622e746bd2ecc87a9108a3d",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e3dfd051b93ff439b693",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.ab48f1c9.chunk.css"
+  },
+  {
+    "revision": "75df2f8bd172b65911f6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.84e543fb.chunk.js"
+  },
+  {
+    "revision": "e3dfd051b93ff439b693",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.825328fa.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
