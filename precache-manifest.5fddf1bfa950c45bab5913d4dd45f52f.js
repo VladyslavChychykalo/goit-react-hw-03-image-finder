9f8b07f48489ce1f3e3102e47cbe1b54967716a9
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-11-19 00:50:26 +0200

    Updates

---
+++ precache-manifest.5fddf1bfa950c45bab5913d4dd45f52f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15670766b13a5440060fb358280693b1",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2f09c2d4fea36e179626",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.18bde93a.chunk.css"
+  },
+  {
+    "revision": "36a4f6ed08e8975a9103",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d4dfc61c.chunk.js"
+  },
+  {
+    "revision": "2f09c2d4fea36e179626",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c417892f.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.67e0fe8a675895745a0a1159abc79b3f.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.5fddf1bfa950c45bab5913d4dd45f52f.js"
 );
 
 self.addEventListener('message', (event) => {
